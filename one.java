38725cde4b047e0264259b154c5ab874b509789b
poorna <poorna123@gmail.com>
2023-08-10 09:42:21 +0530

    change3

--- one.java
+++ one.java
@@ -2,7 +2,8 @@ class helloworld{
 	public static void main(String[]args)
 	{
 		System.out.println("Siddaganga Polytechnic")
-                System.out.println("Batwadi")	
+                System.out.println("Batwadi")
+		System.out.println("Tumakuru")
 	}
 }
 
